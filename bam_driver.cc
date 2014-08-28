a4242d3c836fe57970883d436ffa9598e60457ae
Melissa Ip <nokami.hybrids@gmail.com>
2014-08-27 21:55:03 -0700

    updated documentation

--- bam_driver.cc
+++ bam_driver.cc
@@ -4,29 +4,44 @@
  *
  * This is the driver for parsing a bam file. The input file must contain
  * all reads for the trio and have the appropriate tags.
- * 
- * Useful commands to view overview, header, and a certain section:
- * samtools idxstats
- * samtools view -H <name>.bam
- * samtools view <name>.bam <chr:pos1-pos2>
- * samtools tview <chr:pos1-pos2> <name>.bam
  *
- * To splice a section out and merge it with other splices:
- * samtools view -b <name>.bam <chr:pos1-pos2> <output>.bam
- * Make read group file for bam1 to bamn, tab separated.
+ * SM refers to the name of the sample that identifies it as belonging to the
+ * child, mother, or father. This will vary depending on the data and must be
+ * known in order to parse out the read data.
+ * 
+ * Useful commands to view overview, header, and a certain region:
+ * samtools idxstats <name>.bam
+ * samtools view -H <name>.bam <chr>:<pos1>-<pos2>
+ * samtools view <name>.bam <chr>:<pos1>-<pos2>
+ * samtools tview <chr>:<pos1>-<pos2> <name>.bam
+ *
+ * To splice a region out and merge it with other splices:
+ * samtools view -b <name>.bam <chr>:<pos1>-<pos2> > <output>.bam
+ *
+ * To output all header data that belong to a certain region:
+ * samtools view -o /home/mip/<header>.txt -H <name>.bam <chr>:<pos1>-<pos2>
+ *
+ * Make read group file for bam1 to bamn, tab separated. <header>.txt can be
+ * parsed by filtering out lines that begin with @RG.
+ *
+ * Skip this step if there is only one bam file that contains the bam
+ * alignments for child, mother, and father.
+ * To merge separate bam files for each family member:
  * samtools merge -rh <rg>.txt <output>.bam <bam1>.bam <bamn>.bam
+ *
+ * To prepare the output for the index:
  * samtools sort <output>.bam <output_sorted>.bam
+ *
+ * To create the index:
  * samtools index <output_sorted>.bam <output>.index
  *
+ * <output_sorted>.bam is accepted as input for a specified region.
+ * 
  * To compile on Herschel without using cmake and include GSL and BamTools:
  * c++ -std=c++11 -L/usr/local/lib -lgsl -lgslcblas -lm -L/home/mip/novo_muta_infinite_sites_model/bamtools/lib -I/home/mip/novo_muta_infinite_sites_model/bamtools/include -lbamtools -I/home/mip/novo_muta_infinite_sites_model/bamtools/src -lbamtools-utils -I/usr/local/include -o bam_driver utility.cc read_dependent_data.cc trio_model.cc bamtools/src/utils/bamtools_pileup_engine.cpp variant_visitor.cc bam_driver.cc
  *
  * To run this file, provide the following command line inputs:
  * ./bam_driver <output>.txt <input>.bam <child SM> <mother SM> <father SM>
- *
- * SM refers to the name of the sample that identifies it as belonging to the
- * child, mother, or father. This will vary depending on the data and must be
- * known in order to parse out the read data.
  */
 #include "sufficient_statistics.h"
 #include "variant_visitor.h"
