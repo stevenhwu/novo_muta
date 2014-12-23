b5f8801a8eb805c9be3b853c1f85ada92a3d3ffe
Melissa Ip <nokami.hybrids@gmail.com>
2014-12-22 20:50:40 -0700

    allow user to update parameters in indiv driver

--- indiv_driver.cc
+++ indiv_driver.cc
@@ -20,8 +20,42 @@ int main() {
   uint16_t G = 0;
   uint16_t T = 0;
 
+  char is_default;
+  cout << "Use default trio model parameters? y/n" << endl;
+  cin.get(is_default);
+  cin.ignore(20, '\n');  // Flush buffer.
+  
+  TrioModel params;
+  if (is_default == 'n') {  // All other values uses default parameters.
+    string rate_str;
+    double rate = 0.0;
+    cout << endl << "Enter population mutation rate (theta):" << endl;
+    getline(cin, rate_str);
+    stringstream rate_ss1(rate_str);
+    rate_ss1 >> rate;
+    params.set_population_mutation_rate(rate);
+
+    cout << endl << "Enter germline mutation rate:" << endl;
+    getline(cin, rate_str);
+    stringstream rate_ss2(rate_str);
+    rate_ss2 >> rate;
+    params.set_germline_mutation_rate(rate);
+
+    cout << endl << "Enter somatic mutation rate:" << endl;
+    getline(cin, rate_str);
+    stringstream rate_ss3(rate_str);
+    rate_ss3 >> rate;
+    params.set_somatic_mutation_rate(rate);
+
+    cout << endl << "Enter sequencing error rate:" << endl;
+    getline(cin, rate_str);
+    stringstream rate_ss4(rate_str);
+    rate_ss4 >> rate;
+    params.set_sequencing_error_rate(rate);
+  }
+
   while (true) {
-    cout << "Please enter the child sequencing read <A C G T>. Example: 0 1 2 3" << endl;
+    cout << endl << "Enter the child sequencing read <A C G T>. Example: 0 1 2 3" << endl;
     getline(cin, child_str);
     stringstream child(child_str);
     child >> A;
@@ -30,7 +64,7 @@ int main() {
     child >> T;
     ReadData child_read = {A, C, G, T};
 
-    cout << endl << "Please enter the mother sequencing read <A,C,G,T>. Example: 0,1,2,3" << endl;
+    cout << endl << "Enter the mother sequencing read <A,C,G,T>. Example: 0,1,2,3" << endl;
     getline(cin, mother_str);
     stringstream mother(mother_str);
     mother >> A;
@@ -39,7 +73,7 @@ int main() {
     mother >> T;
     ReadData mother_read = {A, C, G, T};
 
-    cout << endl << "Please enter the father sequencing read <A,C,G,T>. Example: 0,1,2,3" << endl;
+    cout << endl << "Enter the father sequencing read <A,C,G,T>. Example: 0,1,2,3" << endl;
     getline(cin, father_str);
     stringstream father(father_str);
     father >> A;
@@ -48,7 +82,6 @@ int main() {
     father >> T;
     ReadData father_read = {A, C, G, T};
 
-    TrioModel params;
     ReadDataVector data = {child_read, mother_read, father_read};
     cout << endl << "P(Mut):\t" << params.MutationProbability(data) << endl << endl;
   }
